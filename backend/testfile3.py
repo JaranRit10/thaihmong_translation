3d8909754999d7e008db5b4960dbb03f33d0934d
arak-up <anurakjarupakapaisan@gmail.com>
2019-09-17 11:09:03 +0700

    debug test.html,buffer sentence and other

--- backend/testfile3.py
+++ backend/testfile3.py
@@ -1,11 +1,15 @@
 
 
 if __name__ == '__main__':
-    a= "เราไปแล้วทำไม"
-    b = a.find(" ")
-    print(b)
-    if(b != -1):
-        a = list(a)
-        a[b]=','
-        a = "".join(a)
-    print(a)
+    import time
+    bb = {"เรา":"peb","เรา":"peb","เรา2":"peb","เรา3":"peb"}
+    for i in range(1000):
+        bb[i]= i+2
+        if(len(bb) >800):
+            print("bb(clear)",bb)
+            bb.clear()
+
+        print(bb)
+    time.sleep(2)
+    bb.clear()
+    print('clear all')
